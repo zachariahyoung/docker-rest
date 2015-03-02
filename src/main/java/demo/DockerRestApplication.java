2f6ce6f834c826f587c6deb017a169cc0d410752
Zachariah Young <zpyoung@gmail.com>
2015-03-01 18:26:57 -0600

    java app

---
+++ src/main/java/demo/DockerRestApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRestApplication.class, args);
+    }
+}
